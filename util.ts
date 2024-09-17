9e315923113a34570fc4d9a50830d630178d5b44
GitHub <noreply@github.com>
2024-09-17 09:05:06 +0000

    chore(deps): Update deno dependencies

--- util.ts
+++ util.ts
@@ -60,7 +60,7 @@ export function chooseEndpoint(
 }
 
 Deno.test("chooseEndpoint", async () => {
-  const { assertEquals } = await import("jsr:@std/assert@1.0.3");
+  const { assertEquals } = await import("jsr:@std/assert@1.0.5");
 
   const basseOption = {
     ollamaEndpoint: "http://localhost:11434",
